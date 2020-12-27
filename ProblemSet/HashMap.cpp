dbd2cf768229cf3549b9a89e8eb5736c3e1163ff
LambdaNotFound <1701752+LambdaNotFound@users.noreply.github.com>
2020-12-27 01:19:19 -0800

    more solutions (#1)
    tagged with two pointers & hash map

--- ProblemSet/HashMap.cpp
+++ ProblemSet/HashMap.cpp
@@ -1,13 +1,76 @@
 #include "HashMap.h"
 
+// 1. Two Sum
+vector<int> HashMap::twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;  // <num, index>
+    vector<int> result;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int delta = target - nums[i];
+        if (map.find(delta) != map.end()) {
+            result.push_back(map[delta]);
+            result.push_back(i);
+        } else {
+            map[nums[i]] = i; // map.insert(make_pair(nums[i], i));
+        }
+    }
+
+    return result;
+}
+
 // 3. Longest Substring Without Repeating Characters
 int HashMap::lengthOfLongestSubstring(string s) {
     int res = 0, left = 0;
-    unordered_map<int, int> hashMap;
-    for (int i=0; i < s.size(); ++i) {
+    unordered_map<char, int> hashMap;
+    for (int i = 0; i < s.size(); ++i) {
         if (hashMap.count(s[i]) && hashMap[s[i]] >= left)
             left = hashMap[s[i]] + 1;
         hashMap[s[i]] = i;
+        res = max(res, i - left + 1);
+    }
+
+    return res;
+}
+
+/*
+   int lengthOfLongestSubstring(string s) {
+   vector<int> dict(256, -1);
+   int maxLen = 0, start = -1;
+
+   for (int i = 0; i != s.length(); i++) {
+    if (dict[s[i]] > start)
+        start = dict[s[i]];
+    dict[s[i]] = i;
+    maxLen     = max(maxLen, i - start);
+   }
+
+   return maxLen;
+   }
+ */
+
+int HashMap::lengthOfLongestSubstringHashSet(string s) {
+    int res = 0, left = 0;
+    unordered_set<char> hashSet;
+    for (int i = 0; i < s.size();) {
+        if (hashSet.count(s[i]))
+            hashSet.erase(s[left++]);
+        else {
+            hashSet.insert(s[i++]);
+            res = max(res, (int)hashSet.size());
+        }
+    }
+
+    return res;
+}
+
+int HashMap::lengthOfLongestSubstringFollowUp(string s, int k) {
+    int res = 0, left = 0;
+    unordered_map<char, queue<int>> hashMap;
+    for (int i = 0; i < s.size(); ++i) {
+        hashMap[s[i]].push(i);
+        if (hashMap[s[i]].size() == k) {
+            left = hashMap[s[i]].front() + 1; hashMap[s[i]].pop();
+        }
         res = max(res, i - left + 1);
     }
 
--- UnitTest/ProblemSet/HashMap.cpp
+++ UnitTest/ProblemSet/HashMap.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-TEST(L3LongestSubstringWithoutRepeatingCharacters, HashMap)
-{
+TEST(L3LongestSubstringWithoutRepeatingCharacters, HashMap) {
     HashMap sln;
     string input = "abcabcbb";
     int expected = 3;
@@ -15,6 +14,35 @@ TEST(L3LongestSubstringWithoutRepeatingCharacters, HashMap)
 
     input = "aa";
     expected = 1;
-    res = sln.lengthOfLongestSubstring(input);
+    res = sln.lengthOfLongestSubstringHashSet(input);
+    EXPECT_EQ(res, expected);
+
+    // follow-up, without repeating k times
+    int k = 3;
+    input = "abcabcbb";
+    expected = 6;
+
+    res = sln.lengthOfLongestSubstringFollowUp(input, k);
+    EXPECT_EQ(res, expected);
+
+    k = 2;
+    input = "cccccc";
+    expected = 1;
+
+    res = sln.lengthOfLongestSubstringFollowUp(input, k);
+    EXPECT_EQ(res, expected);
+}
+
+TEST(L170TwoSumIII, HashMap) {
+    HashMap::TwoSum sln;
+    sln.add(2);
+    bool res = sln.find(4);
+    bool expected = false;
+    EXPECT_EQ(res, expected);
+
+    sln.add(3);
+    sln.add(1);
+    res = sln.find(3);
+    expected = true;
     EXPECT_EQ(res, expected);
 }
