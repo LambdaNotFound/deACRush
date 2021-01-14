59a879f2f92e76cbd6f345e3bc3456cf58394372
LambdaNotFound <1701752+LambdaNotFound@users.noreply.github.com>
2021-01-13 19:35:07 -0800

    Leetcode (#12)
    * more solutions
    
    tagged with two pointers & hash map
    
    * more sln
    
    priority_queue<T>, min-heap, max-heap
    
    * Top K Frequent Elements
    
    * merge sort
    
    merge sort with STL utils
    
    * Programming Pearls
    
    bit vector
    
    * misc
    
    * merge sort linked lists
    
    * merge sort linked list
    
    * math tricks
    
    238. Product of Array Except Self
    
    updated cmake file to support inspecting STL containers during debug
    
    * more solutions
    
    sliding window
    
    * Update MergeSort.h
    
    * more solutions
    
    * more solutions, add gmock
    
    backtracking
    
    * more solutions
    
    binary search
    
    * next permutation
    
    * permutation sequence
    
    * revert delete
    
    * Trie

--- DataStructure/Trie.cpp
+++ DataStructure/Trie.cpp
@@ -1,1 +1,61 @@
 #include "Trie.h"
+
+/** Inserts a word into the trie. */
+void Trie::insert(string word) {
+    TrieNode* cur = m_root;
+    for (auto const& c : word) {
+        if (!cur->findChild(c)) {
+            TrieNode* tmp = new TrieNode(c);
+            cur->addChild(tmp);
+        }
+        cur = cur->findChild(c);
+    }
+    cur->markEndOfWord();
+}
+
+/** Returns if the word is in the trie. */
+bool Trie::search(string word) {
+    TrieNode* cur = m_root;
+    for (auto const& c: word) {
+        cur = cur->findChild(c);
+        if (!cur)
+            return false;
+    }
+    return cur->isEndOfWord();
+}
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+bool Trie::startsWith(string prefix) {
+    TrieNode* cur = m_root;
+    for (auto const& c: prefix) {
+        cur = cur->findChild(c);
+        if (!cur)
+            return false;
+    }
+    return true;
+}
+
+/** follow-up: delete a word from the trie **/
+bool Trie::deleteWord(string word) {
+    TrieNode* cur = m_root;
+    stack<TrieNode*> s;
+    for (auto const& c: word) {
+        cur = cur->findChild(c);
+        if (!cur)
+            return false;
+        s.push(cur);
+    }
+    cur->unmarkEndOfWord();
+
+    if (!cur->hasChild()) {
+        while (!s.empty()) {
+            TrieNode* tmp = s.top(); s.pop();
+            tmp->removeChild(cur);
+            cur = tmp;
+            if (tmp->hasChild())
+                break;
+        }
+    }
+
+    return true;
+}
--- DataStructure/Trie.h
+++ DataStructure/Trie.h
@@ -1,4 +1,93 @@
 #ifndef ACRUSH_TRIE_H
 #define ACRUSH_TRIE_H
 
+#include "StandardLibrary.h"
+
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode(const char c) : m_val(c), m_is_end_of_word(false) {
+    }
+
+    TrieNode() : TrieNode('\0') {
+    }
+
+    ~TrieNode() {
+      for (auto const& [key, value] : m_child_node_map)
+        delete value;
+    }
+
+    char value() const {
+        return m_val;
+    }
+
+    bool isEndOfWord() const {
+        return m_is_end_of_word;
+    }
+
+    bool hasChild() const {
+        return m_child_node_map.size() != 0;
+    }
+
+    TrieNode* findChild(const char c) {
+        if (m_child_node_map.count(c))
+            return m_child_node_map[c];
+        else
+            return nullptr;
+    }
+
+    void addChild(TrieNode* child) {
+        m_child_node_map[child->value()] = child;
+    }
+
+    void removeChild(TrieNode* child) {
+        m_child_node_map.erase(child->value());
+    }
+
+    void markEndOfWord() {
+        m_is_end_of_word = true;
+    }
+
+    void unmarkEndOfWord() {
+        m_is_end_of_word = false;
+    }
+
+private:
+    char m_val;
+    bool m_is_end_of_word;
+    unordered_map<char, TrieNode*> m_child_node_map;
+};
+
+/*
+ * 208. Implement Trie (Prefix Tree)
+ *
+ */
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        m_root = new TrieNode();
+    }
+
+    ~Trie() {
+        delete m_root;
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word);
+
+    /** Returns if the word is in the trie. */
+    bool search(string word);
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix);
+
+    /** follow-up: delete a word from the trie **/
+    bool deleteWord(string word);
+
+private:
+    TrieNode* m_root;
+};
+
 #endif //ACRUSH_TRIE_H
