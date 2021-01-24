988129d00e0ed866afeff6151c0390cf500906b5
LambdaNotFound <1701752+LambdaNotFound@users.noreply.github.com>
2021-01-23 17:43:55 -0800

    Leetcode (#14)
    * more solutions
    
    tagged with two pointers & hash map
    
    * more sln
    
    priority_queue<T>, min-heap, max-heap
    
    * Top K Frequent Elements
    
    * merge sort
    
    merge sort with STL utils
    
    * Programming Pearls
    
    bit vector
    
    * misc
    
    * merge sort linked lists
    
    * merge sort linked list
    
    * math tricks
    
    238. Product of Array Except Self
    
    updated cmake file to support inspecting STL containers during debug
    
    * more solutions
    
    sliding window
    
    * Update MergeSort.h
    
    * more solutions
    
    * more solutions, add gmock
    
    backtracking
    
    * more solutions
    
    binary search
    
    * next permutation
    
    * permutation sequence
    
    * revert delete
    
    * Trie
    
    * trie def bug fix
    
    * serialize binary tree
    
    DFS approach and iterative approach are not equivalent
    
    * Word Ladder II

--- ProblemSet/Tree.h
+++ ProblemSet/Tree.h
@@ -14,6 +14,20 @@ class Tree {
  */
 
 /*
+ * 126. Word Ladder II
+ *
+ * Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord.
+ *
+ * i. BFS
+ * ii. Bidirectional BFS
+ */
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList);
+
+public:
+    vector<vector<string>> findLadders2(string beginWord, string endWord, vector<string>& wordList);
+
+/*
  * 226. Invert Binary Tree
  *
  * i. BFS
--- UnitTest/Tree.cpp
+++ UnitTest/Tree.cpp
@@ -1,3 +1,4 @@
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #include "TreeNode.h"
@@ -18,4 +19,17 @@ TEST(L572SubtreeOfAnotherTree, TreeRecursion) {
     bool expected = false;
     bool result = sln.isSubtreeSerialize(p.get(), q.get());
     EXPECT_EQ(result, expected);
+}
+
+TEST(L126WordLadderII, BFS) {
+    Tree sln;
+    string beginWord = "hit", endWord = "cog";
+    vector<string> wordList = { "hot","dot","dog","lot","log","cog" };
+    vector<vector<string> > expected = { {"hit","hot","dot","dog","cog"}, {"hit","hot","lot","log","cog"} };
+
+    auto result = sln.findLadders(beginWord, endWord, wordList);
+    EXPECT_THAT(result, ::testing::UnorderedElementsAreArray(expected));
+
+    result = sln.findLadders2(beginWord, endWord, wordList);
+    EXPECT_THAT(result, ::testing::UnorderedElementsAreArray(expected));
 }
