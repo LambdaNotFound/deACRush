e50ff73bd1bfe4691a3e329621dfad977ae8c142
LambdaNotFound <yiqyudev@gmail.com>
2021-02-04 23:49:18 -0800

    Majority Element

--- ProblemSet/DivideAndConquer.cpp
+++ ProblemSet/DivideAndConquer.cpp
@@ -1,1 +1,33 @@
 #include "DivideAndConquer.h"
+
+// 169. Majority Element
+int DivideAndConquer::majorityElement(vector<int>& nums) {
+    return majorityElementHelper(nums, 0, (int)nums.size());
+}
+int DivideAndConquer::majorityElementHelper(vector<int>& nums, int left, int right) {
+    if (left == right - 1)
+        return nums[left];
+
+    int mid = left + (right - left) / 2;
+    int leftMajority = majorityElementHelper(nums, left, mid);
+    int rightMajority = majorityElementHelper(nums, mid, right);
+
+    if (leftMajority == rightMajority)
+        return leftMajority;
+    else {
+        int leftMajorityCountInRight = count(nums, mid, right, leftMajority);
+        int rightMajorityCountInLeft = count(nums, left, mid, rightMajority);
+
+        if (leftMajorityCountInRight > rightMajorityCountInLeft)
+            return leftMajority;
+        else
+            return rightMajority;
+    }
+}
+int DivideAndConquer::count(vector<int>& nums, int left, int right, int target) {
+    int count = 0;
+    for (int i = left; i < right; ++i)
+        if (nums[i] == target)
+            ++count;
+    return count;
+}
--- ProblemSet/DivideAndConquer.h
+++ ProblemSet/DivideAndConquer.h
@@ -1,6 +1,28 @@
 #ifndef ACRUSH_DIVIDEANDCONQUER_H
 #define ACRUSH_DIVIDEANDCONQUER_H
 
+#include "StandardLibrary.h"
 
+using namespace std;
+
+class DivideAndConquer {
+
+/*
+ * 169. Majority Element
+ *
+ * Given an array nums of size n, return the majority element.
+ *
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ *
+ * i. BitManipulation <= LIKE
+ * ii. DivideAndConquer
+ */
+public:
+    int majorityElement(vector<int>& nums);
+private:
+    int majorityElementHelper(vector<int>& nums, int left, int right);
+    int count(vector<int>& nums, int left, int right, int target);
+
+};
 
 #endif //ACRUSH_DIVIDEANDCONQUER_H
