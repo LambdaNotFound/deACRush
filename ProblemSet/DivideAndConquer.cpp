38ab262cfd6115e617736bf718ab6e6cf2fcb971
LambdaNotFound <1701752+LambdaNotFound@users.noreply.github.com>
2021-02-12 00:55:23 -0800

    Leetcode (#21)
    * more solutions
    
    tagged with two pointers & hash map
    
    * more sln
    
    priority_queue<T>, min-heap, max-heap
    
    * Top K Frequent Elements
    
    * merge sort
    
    merge sort with STL utils
    
    * Programming Pearls
    
    bit vector
    
    * misc
    
    * merge sort linked lists
    
    * merge sort linked list
    
    * math tricks
    
    238. Product of Array Except Self
    
    updated cmake file to support inspecting STL containers during debug
    
    * more solutions
    
    sliding window
    
    * Update MergeSort.h
    
    * more solutions
    
    * more solutions, add gmock
    
    backtracking
    
    * more solutions
    
    binary search
    
    * next permutation
    
    * permutation sequence
    
    * revert delete
    
    * Trie
    
    * trie def bug fix
    
    * serialize binary tree
    
    DFS approach and iterative approach are not equivalent
    
    * Word Ladder II
    
    * clean up linked list
    
    * Majority Element
    
    * Bit Manipulation
    
    * provide a suitable hash function for your key type
    
    * DP
    
    * work break
    
    * divide and conquer

--- ProblemSet/DivideAndConquer.cpp
+++ ProblemSet/DivideAndConquer.cpp
@@ -1,4 +1,36 @@
 #include "DivideAndConquer.h"
+#include "../DataStructure/TreeNode.h"
+
+// 95. Unique Binary Search Trees II
+vector<TreeNode*> DivideAndConquer::generateTrees(int n) {
+    unordered_map<string, vector<TreeNode*>> memo;
+    return generateTreesHelper(1, n, memo);
+}
+vector<TreeNode*> DivideAndConquer::generateTreesHelper(int begin, int end, unordered_map<string, vector<TreeNode*>>& memo) {
+    if (begin > end)
+        return { nullptr };
+
+    string key = to_string(begin) + '-' + to_string(end);
+    if (memo.count(key))
+        return memo[key];
+
+    vector<TreeNode*> res;
+    for (int i = begin; i <= end; ++i) {
+        vector<TreeNode*> leftChild = generateTreesHelper(begin, i - 1, memo);
+        vector<TreeNode*> rightChild = generateTreesHelper(i + 1, end, memo);
+        for (TreeNode* left : leftChild)
+            for (TreeNode* right : rightChild) {
+                TreeNode* root = new TreeNode(i);
+                root->left = left;
+                root->right = right;
+                res.push_back(root);
+            }
+    }
+
+    memo[key] = res;
+    return res;
+}
+
 
 // 169. Majority Element
 int DivideAndConquer::majorityElement(vector<int>& nums) {
@@ -30,4 +62,37 @@ int DivideAndConquer::count(vector<int>& nums, int left, int right, int target) 
         if (nums[i] == target)
             ++count;
     return count;
+}
+
+// 241. Different Ways to Add Parentheses
+vector<int> DivideAndConquer::diffWaysToCompute(string input) {
+    unordered_map<string, vector<int>> memo;
+    return diffWaysToComputeHelper(input, memo);
+}
+vector<int> DivideAndConquer::diffWaysToComputeHelper(const string& s, unordered_map<string, vector<int>>& memo) {
+    if (memo.count(s))
+        return memo[s];
+
+    vector<int> res;
+    for (int i = 0; i < s.size() - 1; ++i) {
+        if (isdigit(s[i]))
+            continue;
+
+        vector<int> left = diffWaysToComputeHelper(s.substr(0, i), memo);
+        vector<int> right = diffWaysToComputeHelper(s.substr(i + 1), memo);
+        for (auto x : left)
+            for (auto y : right)
+                if (s[i] == '+')
+                    res.push_back(x + y);
+                else if (s[i] == '-')
+                    res.push_back(x - y);
+                else
+                    res.push_back(x * y);
+    }
+
+    if (res.size() == 0)
+        res.push_back(stoi(s));
+    memo[s] = res;
+
+    return res;
 }
